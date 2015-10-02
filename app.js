977bb810c517c34ea7fffc8c421cf2d226fb0940
Captain Nik Buckman <ZeroGodForce@EDI.local>
2015-10-02 16:14:34 +0100

    Final commit prior to UI

--- app.js
+++ app.js
@@ -1,92 +1,56 @@
-(function(){
-    //start file
+(function () {
+	//start file
 
-    var app = angular.module('thortHub', []);
-
-
-
-    app.controller('gitHubController', ['$scope','$http', function($scope,$http) {
-
-        $scope.username = "ZeroGodForce";
-        $http.get("https://api.github.com/users/"+$scope.username)
-            .success(function(data) {
-                //console.log(data);
-                $scope.userData = data;
+	var app = angular.module('thortHub', []);
 
 
+	app.controller('gitHubController', ['$scope', '$http', function ($scope, $http) {
 
-                $http.get($scope.userData.repos_url)
-                    .success(function(data){
-                        $scope.repoData = data;
-                    });
+		$scope.username = "ZeroGodForce";
+		$http.get("https://api.github.com/users/" + $scope.username)
+			.success(function (data) {
+				$scope.userData = data;
 
-            });
-
-
-    }]);
-
-
-    app.controller('repoSearchController', ['$scope','$http', function($scope,$http) {
-
-
-        //if($scope.searchRepo === undefined){
-        //    $scope.searchRepo = "bootstrap";
-        //    fetch();
-        //}
+				// Get users repos
+				$http.get($scope.userData.repos_url)
+					.success(function (data) {
+						$scope.repoData = data;
+					});
+			});
+	}]);
 
 
+	app.controller('repoSearchController', ['$scope', '$http', function ($scope, $http) {
 
-        var pendingTask;
-
-        $scope.change = function(){
-            if(pendingTask) {
-                clearTimeout(pendingTask);
-            }
-            pendingTask = setTimeout(fetch, 800);
-        };
-
-
-
-        function fetch() {
-            //$http.get("http://www.omdbapi.com/?t=" +
-            //$scope.search + "&tomatoes=true&plot=full")
-            //    .success(function(response){$scope.details = response;});
-            //
-            //$http.get("http://www.omdbapi.com/?s=" + $scope.search)
-            //    .success(function(response){$scope.related = response;});
+		// Wait a bit for the user to finish typing before searching
+		var pendingTask;
+		$scope.change = function () {
+			if (pendingTask) {
+				clearTimeout(pendingTask);
+			}
+			pendingTask = setTimeout(fetch, 600);
+		};
 
 
+		// search github for the repository
+		function fetch() {
+			$http.get("https://api.github.com/search/repositories?q=" + $scope.searchRepo)
+				.success(function (response) {
+					console.log(response);
+					$scope.results = response;
+				});
+		}
 
-            $http.get("https://api.github.com/search/repositories?q="+$scope.searchRepo)
-                .success(function(response) {
-                    console.log(response);
-                    $scope.results = response;
-                });
+		// Search again if the input changes (user searches for something else)
+		$scope.update = function (repo) {
+			$scope.searchRepo = repo.full_name;
+			$scope.change();
+		};
 
-
-        }
-
-        $scope.update = function(repo){
-            $scope.searchRepo = repo.full_name;
-            $scope.change();
-        };
-
-        $scope.select = function(){
-            this.setSelectionRange(0, this.value.length);
-        }
-
-    }]);
-
-
-
-
-
-
-
-
-
-
-
+		$scope.select = function () {
+			this.setSelectionRange(0, this.value.length);
+		}
+	}]);
 
 
 //end file
