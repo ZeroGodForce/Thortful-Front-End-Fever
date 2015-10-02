50fe28c45077da5ea2be80f79beaaf501f74de38
Captain Nik Buckman <ZeroGodForce@EDI.local>
2015-10-02 15:11:28 +0100

    repo search working

--- app.js
+++ app.js
@@ -28,21 +28,52 @@
 
     app.controller('repoSearchController', ['$scope','$http', function($scope,$http) {
 
-        $scope.reponame = "bootstrap";
-        $http.get("https://api.github.com/search/repositories?q="+$scope.reponame)
-            .success(function(data) {
-                console.log(data);
-                $scope.repoSearchData = data;
+
+        //if($scope.searchRepo === undefined){
+        //    $scope.searchRepo = "bootstrap";
+        //    fetch();
+        //}
 
 
 
-                //$http.get($scope.userData.repos_url)
-                //    .success(function(data){
-                //        $scope.repoData = data;
-                //    });
+        var pendingTask;
 
-            });
+        $scope.change = function(){
+            if(pendingTask) {
+                clearTimeout(pendingTask);
+            }
+            pendingTask = setTimeout(fetch, 800);
+        };
 
+
+
+        function fetch() {
+            //$http.get("http://www.omdbapi.com/?t=" +
+            //$scope.search + "&tomatoes=true&plot=full")
+            //    .success(function(response){$scope.details = response;});
+            //
+            //$http.get("http://www.omdbapi.com/?s=" + $scope.search)
+            //    .success(function(response){$scope.related = response;});
+
+
+
+            $http.get("https://api.github.com/search/repositories?q="+$scope.searchRepo)
+                .success(function(response) {
+                    console.log(response);
+                    $scope.results = response;
+                });
+
+
+        }
+
+        $scope.update = function(repo){
+            $scope.searchRepo = repo.full_name;
+            $scope.change();
+        };
+
+        $scope.select = function(){
+            this.setSelectionRange(0, this.value.length);
+        }
 
     }]);
 
